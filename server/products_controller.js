af10affd4a269b1d706380d7d9d86a320b0e741a
irisi1223 <jbushman442@gmail.com>
2020-07-21 12:20:03 -0600

    updated proj

--- server/products_controller.js
+++ server/products_controller.js
@@ -1,54 +1,58 @@
 module.exports= {
-    create: (req, res) =>{
+    create: (req, res, next) =>{
         const dbInstance = req.app.get('db');
         const {name, description, price, image_url} = req.body;
 
         dbInstance.create_product([name, description, price, image_url])
-        .then(() => res.setStatuse(200))
-        .catch(err => {
+          .then(() => res.sendStatuse(200))
+          .catch(err => {
             res.status(500).send({errorMessage: "Sorry we messed up!!"});
             console.log(err)
         });
     },
-    getOne: (req, res) =>{
+    getOne: (req, res, next) =>{
         const dbInstance = req.app.get('db');
         const {id} = req.prams;
-        dbInstance.read_products(id)
-        .then(product => res.status(200).send(product))
-        .catch(err => {
+
+        dbInstance.read_product(id)
+          .then(product => res.status(200).send(product))
+          .catch(err => {
             res.status(500).send({errorMessage:"Sorry we messed up!!"})
             console.log(err)
         })
     },
-    getAll: (req, res) =>{
+    getAll: (req, res, next) =>{
         const dbInstance = req.app.get('db');
+
         dbInstance.read_products()
-      .then(products => res.status(200).send(products))
-      .catch(err => {
-        res.status(500).send({errorMessage: "Sorry we messed up!!"});
-        console.log(err)
-      } );
+          .then(products => res.status(200).send(products))
+          .catch(err => {
+            res.status(500).send({errorMessage: "Sorry we messed up!!"});
+            console.log(err)
+          } );
   
     },
-    update: (req, res) =>{
+    update: (req, res, next) =>{
         const dbInstance = req.app.get('db');
         const {params, query} = req;
+
         dbInstance.update_product([params.id, query.desc])
-      .then( () => res.sendStatus(200) )
-      .catch(err => {
-        res.status(500).send({errorMessage:"Sorry we messed up!!"});
-        console.log(err)
-      } );
+          .then( () => res.sendStatus(200) )
+          .catch(err => {
+            res.status(500).send({errorMessage:"Sorry we messed up!!"});
+            console.log(err)
+        } );
   
     },
-    delete: (req, res) =>{
+    delete: (req, res, next) =>{
         const dbInstance = req.app.get('db');
         const {id} = req.params;
+
         dbInstance.delete_product(id)
-      .then( () => res.sendStatus(200) )
-      .catch(err => {
-        res.status(500).send({errorMessage: "Sorry we messed up!!"});
-        console.log(err)
-      } );
+          .then( () => res.sendStatus(200) )
+          .catch(err => {
+            res.status(500).send({errorMessage: "Sorry we messed up!!"});
+            console.log(err)
+        } );
     }
 };
